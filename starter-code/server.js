d8181f9c81ef5691452528f0c62e9d771050017a
mstclair <stclairm@spu.edu>
2017-08-29 15:37:34 -0700

    fixed handle bars and added callback function

--- starter-code/server.js
+++ starter-code/server.js
@@ -4,10 +4,12 @@ const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://localhost:5432/kilovolt'; // TODO: Don't forget to set your own conString
+// const conString = 'postgres://localhost:5432/kilovolt';
+const conString = 'postgres://mstclair:dogfood1@postgres:5432';
+// done: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
